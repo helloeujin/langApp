4aaed2048a49d6fcc9ee3dc2c1e20affccddfeac
Youjin <meggonagul@gmail.com>
2022-10-31 10:28:04 +0900

    card swipe animation

--- App.js
+++ App.js
@@ -12,8 +12,8 @@ const Container = styled.View`
 
 const Card = styled(Animated.createAnimatedComponent(View))`
   background-color: white;
-  width: 300px;
-  height: 300px;
+  width: 260px;
+  height: 260px;
   justify-content: center;
   align-items: center;
   border-radius: 12px;
@@ -28,24 +28,42 @@ export default function App() {
       onPanResponderMove: (_, { dx }) => {
         position.setValue(dx);
       },
-      onPanResponderRelease: () => {
-        Animated.parallel([
-          onPressOut,
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -250) {
           Animated.spring(position, {
-            toValue: 0,
+            toValue: -500,
             useNativeDriver: true,
-          }),
-        ]).start();
+          }).start();
+        } else if (dx > 250) {
+          Animated.spring(position, {
+            toValue: 400,
+            useNativeDriver: true,
+          }).start();
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
     })
   ).current;
 
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["-15deg", "15deg"],
+    // extrapolate: "clamp",
+  });
+
+  // position.addListener(() => console.log(position, rotation));
+
   const onPressIn = () =>
     Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
   const onPressOut = Animated.spring(scale, {
     toValue: 1,
+    useNativeDriver: true,
+  });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
     useNativeDriver: true,
   });
 
@@ -54,7 +72,11 @@ export default function App() {
       <Card
         {...panResponder.panHandlers}
         style={{
-          transform: [{ scale }, { translateX: position }],
+          transform: [
+            { scale },
+            { translateX: position },
+            { rotateZ: rotation },
+          ],
         }}
       >
         <Ionicons name="pizza" color="#192a56" size={98} />
