cb591d03cac40bdcea79620fbdda29fe5dde3c5e
Youjin <meggonagul@gmail.com>
2022-10-31 19:17:12 +0900

    drag drop addded

--- DragDrop.js
+++ DragDrop.js
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 import icons from "./icons";
+import { Animated, Easing, PanResponder } from "react-native";
+import { View } from "react-native";
 
 const BLACK_COLOR = "#1e272e";
 const GREY = "#485460";
@@ -13,8 +15,143 @@ const Container = styled.View`
   background-color: ${BLACK_COLOR};
 `;
 
+const Edge = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WordContainer = styled(Animated.createAnimatedComponent(View))`
+  width: 100px;
+  height: 100px;
+  background-color: ${GREY};
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+`;
+
+const Word = styled.Text`
+  font-size: 38px;
+  color: ${(props) => props.color};
+  font-weight: 500;
+`;
+
+const Center = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+`;
+
+const IconCard = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  padding: 5px 10px;
+  border-radius: 10px;
+`;
+
 const DragDrop = () => {
-  return <Container></Container>;
+  // Values
+  const opacity = useRef(new Animated.Value(1)).current;
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const scaleOne = position.y.interpolate({
+    inputRange: [-300, -80],
+    outputRange: [2, 1],
+    extrapolate: "clamp",
+  });
+  const scaleTwo = position.y.interpolate({
+    inputRange: [80, 300],
+    outputRange: [1, 2],
+    extrapolate: "clamp",
+  });
+
+  // Animations
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.9,
+    useNativeDriver: true,
+  });
+
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+
+  const goHome = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+
+  const onDropScale = Animated.timing(scale, {
+    toValue: 0,
+    duration: 50,
+    easing: Easing.linear,
+    useNativeDriver: true,
+  });
+
+  const onDropOpacity = Animated.timing(opacity, {
+    toValue: 0,
+    duration: 50,
+    easing: Easing.linear,
+    useNativeDriver: true,
+  });
+
+  // Pan Responder
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx, dy }) => {
+        position.setValue({ x: dx, y: dy });
+      },
+      onPanResponderGrant: () => {
+        onPressIn.start();
+      },
+      onPanResponderRelease: (_, { dy }) => {
+        if (dy < -250 || dy > 250) {
+          Animated.sequence([
+            Animated.parallel([onDropOpacity, onDropScale]),
+            Animated.timing(position, {
+              toValue: 0,
+              duration: 100,
+              easing: Easing.linear,
+              useNativeDriver: true,
+            }),
+          ]).start();
+        } else {
+          Animated.parallel([onPressOut, goHome]).start();
+        }
+      },
+    })
+  ).current;
+
+  // State
+
+  return (
+    <Container>
+      <Edge>
+        <WordContainer style={{ transform: [{ scale: scaleOne }] }}>
+          <Word color={GREEN}>알아</Word>
+        </WordContainer>
+      </Edge>
+
+      <Center>
+        <IconCard
+          {...panResponder.panHandlers}
+          style={{
+            opacity,
+            transform: [...position.getTranslateTransform(), { scale }],
+          }}
+        >
+          <Ionicons name="beer" color={GREY} size={76} />
+        </IconCard>
+      </Center>
+
+      <Edge>
+        <WordContainer style={{ transform: [{ scale: scaleTwo }] }}>
+          <Word color={RED}>몰라</Word>
+        </WordContainer>
+      </Edge>
+    </Container>
+  );
 };
 
 export default DragDrop;
